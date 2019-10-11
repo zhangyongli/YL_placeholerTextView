1bd58fc37ad51ec161f4b2c0a970e23922ea9bc6
zhangyongli <375473898@qq.com>
2019-10-11 14:52:59 +0800

    CUSTOM PLACEHOLOER TEXTVIEW
    user the textView with placeholer

---
+++ YL_PlaceholderTextView.m
@@ -0,0 +1,108 @@
+
+
+#import "YL_PlaceholderTextView.h"
+
+@interface YL_PlaceholderTextView()
+
+@property (nonatomic, weak) UILabel *placeholderLabel;
+@end
+
+@implementation YL_PlaceholderTextView
+
+- (UILabel *)placeholderLabel
+{
+    if (!_placeholderLabel) {
+        // 添加一个用来显示占位文字的label
+        UILabel *placeholderLabel = [[UILabel alloc] init];
+        placeholderLabel.numberOfLines = 0;
+        placeholderLabel.origin.x = 4;
+        placeholderLabel.origin.y = 7;
+        [self addSubview:placeholderLabel];
+        _placeholderLabel = placeholderLabel;
+    }
+    return _placeholderLabel;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        self.alwaysBounceVertical = YES;
+        self.font = [UIFont systemFontOfSize:15];
+        
+        // 默认的占位文字颜色
+        self.placeholderColor = [UIColor grayColor];
+        
+        // 监听文字改变
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange) name:UITextViewTextDidChangeNotification object:self];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+/**
+ * 监听文字改变
+ */
+- (void)textDidChange
+{
+    // 只要有文字, 就隐藏占位文字label
+    self.placeholderLabel.hidden = self.hasText;
+}
+
+/**
+ * 更新占位文字的尺寸
+ */
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    self.placeholderLabel.width = self.width - 2 * self.placeholderLabel.x;
+    [self.placeholderLabel sizeToFit];
+}
+
+#pragma mark - 重写setter
+- (void)setPlaceholderColor:(UIColor *)placeholderColor
+{
+    _placeholderColor = placeholderColor;
+    
+    self.placeholderLabel.textColor = placeholderColor;
+}
+
+- (void)setPlaceholder:(NSString *)placeholder
+{
+    _placeholder = [placeholder copy];
+    
+    self.placeholderLabel.text = placeholder;
+    
+    [self setNeedsLayout];
+}
+
+- (void)setFont:(UIFont *)font
+{
+    [super setFont:font];
+    
+    self.placeholderLabel.font = font;
+    
+    [self setNeedsLayout];
+}
+
+- (void)setText:(NSString *)text
+{
+    [super setText:text];
+    
+    [self textDidChange];
+}
+
+- (void)setAttributedText:(NSAttributedString *)attributedText
+{
+    [super setAttributedText:attributedText];
+    
+    [self textDidChange];
+}
+
+
+@end
